4c35b214e469473a76284270d3daf3b61876d10a
Remilia Scarlet <remilia.1505@gmail.com>
2017-02-16 12:32:01 +0700

    fonts: Fix my derp
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- data/fonts/fonts.mk
+++ data/fonts/fonts.mk
@@ -17,7 +17,7 @@
 PRODUCT_COPY_FILES := \
     frameworks/base/data/fonts/fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/fonts.xml \
     frameworks/base/data/fonts/big-noodle.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/big-noodle.ttf \
-    frameworks/base/data/fonts/big-noodle.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/big-noodleItalic.ttf
+    frameworks/base/data/fonts/big-noodleItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/big-noodleItalic.ttf
 
 PRODUCT_PACKAGES := \
     DroidSansFallback.ttf \
