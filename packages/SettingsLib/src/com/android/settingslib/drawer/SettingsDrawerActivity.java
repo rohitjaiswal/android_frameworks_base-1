9e5260b3bdb6d7da3fc21194f18fff4238a250be
Remilia Scarlet <remilia.1505@gmail.com>
2017-06-12 03:46:31 +0000

    Revert "Fix lag when opening an entry from the Settings left drawer"
    This reverts commit ee5bcff53645a5f61367f81d16c520e156d76567.
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/SettingsDrawerActivity.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/SettingsDrawerActivity.java
@@ -76,7 +76,6 @@ public class SettingsDrawerActivity extends Activity {
     private SettingsDrawerAdapter mDrawerAdapter;
     private FrameLayout mContentHeaderContainer;
     private DrawerLayout mDrawerLayout;
-    private boolean mOpenTileFromLeftDrawer;
     private boolean mShowingMenu;
     private UserManager mUserManager;
 
@@ -117,8 +116,7 @@ public class SettingsDrawerActivity extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(android.widget.AdapterView<?> parent, View view, int position,
                     long id) {
-                position = position - 1;
-                mOpenTileFromLeftDrawer = true;
+               position = position - 1;
                 onTileClicked(mDrawerAdapter.getTile(position));
             }
         });
@@ -315,13 +313,7 @@ public class SettingsDrawerActivity extends Activity {
     }
 
     public boolean openTile(Tile tile) {
-        if (mOpenTileFromLeftDrawer) {
-            // if we open a tile from the left drawer, don't close/animate the drawer
-            // so it will go away with the old activity animation, avoiding lags
-            mOpenTileFromLeftDrawer = false;
-        } else {
-            closeDrawer();
-        }
+        closeDrawer();
         if (tile == null) {
             startActivity(new Intent(Settings.ACTION_SETTINGS).addFlags(
                     Intent.FLAG_ACTIVITY_CLEAR_TASK));
